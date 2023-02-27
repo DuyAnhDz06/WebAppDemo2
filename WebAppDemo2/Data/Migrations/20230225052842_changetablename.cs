bc645ca35327c2f23fd4f80316267e9065149777
DuyAnhDz <duyanh20gg@gmail.com>
2023-02-27 10:31:36 +0700

    Add project files.

---
+++ WebAppDemo2/Data/Migrations/20230225052842_changetablename.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppDemo2.Data.Migrations
+{
+    public partial class changetablename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Category");
+        }
+    }
+}
